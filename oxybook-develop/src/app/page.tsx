1eb501d984cb1594f7929a1115f1c593eb926dab
MichaelOwen2509 <michaelowen2509@gmail.com>
2026-05-08 09:38:54 -0300

    referencias

--- oxybook-develop/src/app/page.tsx
+++ oxybook-develop/src/app/page.tsx
@@ -1,11 +1,12 @@
 import { SelectedBooksList } from "@/presentation/shared/components/livros/SelectedBooksList";
 import Image from "next/image";
+import BookLoan from "./emprestimo-livro/page";
 
 export default function Home() {
   return (
     <div>
       <h1>pagina inicial</h1>
-     <SelectedBooksList /> 
+     <BookLoan /> 
     </div>
     
   );
